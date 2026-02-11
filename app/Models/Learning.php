d2cc276a9321a1db31c998ec135819313cdbc2b4
abghifareihand <info.abghifareihan@gmail.com>
2026-02-11 22:47:55 +0700

    add hasfactory

--- app/Models/Learning.php
+++ app/Models/Learning.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Learning extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'type',
         'title',
